521170d1dba2df34b61da8e1e8d818735c4154de
Yana <oyana1111@gmail.com>
2023-10-27 15:25:09 +0300

    Добавьте файлы проекта.

---
+++ Lab2/Lab2.cpp
@@ -0,0 +1,68 @@
+﻿#ifndef UNICODE 
+#define UNICODE 
+#endif
+
+#include <Windows.h> 
+#include<strsafe.h> 
+#include <vector> 
+#include <string> 
+#include <fstream> 
+
+using namespace std;
+HWND hWnd;
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow)
+{
+    WNDCLASSEX wcex;
+    MSG msg;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_VREDRAW | CS_HREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = NULL;
+    wcex.hCursor = LoadCursorW(NULL, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = NULL;
+    wcex.lpszClassName = L"Lab2";
+    wcex.hIconSm = NULL;
+
+    if (!RegisterClassEx(&wcex))
+    {
+        return 0;
+    }
+
+    hWnd = CreateWindowEx(0, L"Lab2", L"SP2", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
+        CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL); 
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    return msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    static int width, height;
+
+    switch (uMsg)
+    {
+    case WM_SIZE:
+        width = LOWORD(lParam);
+        height = HIWORD(lParam);
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    }
+    return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
